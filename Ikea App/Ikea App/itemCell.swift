8b502f8aefc059501571bab96ee602e9d722b3e7
ZaynahB <34633371+ZaynahB@users.noreply.github.com>
2018-04-16 13:54:59 -0400

    Add files via upload

---
+++ Ikea App/Ikea App/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  Ikea App
+//
+//  Created by Zaynah Bhanji on 2018-04-15.
+//  Copyright © 2018 Zaynah Bhanji. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
